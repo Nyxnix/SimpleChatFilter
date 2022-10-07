92ba1f3914925ca573a684e6456d3ee8d9ca937b
JoshQM <57198384+JoshQM@users.noreply.github.com>
2022-10-07 09:34:04 -0500

    1.1
    - Fixed case sensitivity issue
    - Added logging to console

--- src/main/java/me/nyxnix/simplechatfilter/SimpleChatFilter.java
+++ src/main/java/me/nyxnix/simplechatfilter/SimpleChatFilter.java
@@ -5,7 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.List;
 
 public final class SimpleChatFilter extends JavaPlugin implements Listener {
@@ -25,17 +24,25 @@ public final class SimpleChatFilter extends JavaPlugin implements Listener {
 
         Player player = event.getPlayer();
         String playerMessage = event.getMessage();
+        String playerMessageOriginal = event.getMessage();
 
         // Pulling strings from config file
         final List<String> words = getConfig().getStringList("words");
 
-        // Main section that replaces swears with asterisks
-        // Pulls strings from config file then if that string is present in the message
-        // It will be replaced with asterisks
+        /* Main section that replaces blacklisted words with asterisks
+           Pulls strings from config file then if that string is present in the message
+           It will be replaced with asterisks */
         for(String listItem : words){
-            if(playerMessage.contains(listItem)){
+            /* There is 100% better way to do this I just haven't figured it out yet
+            So for now im just comparing every message in lowercase to the list
+            which probably uses more resources than is needed :(
+             */
+            if(playerMessage.toLowerCase().contains(listItem)){
+                playerMessage = playerMessage.toLowerCase();
                 playerMessage = playerMessage.replace(listItem, "***");
                 event.setMessage(playerMessage);
+                // log infraction to console
+                System.out.println("Blacklisted word found! Player: "+player.getPlayerListName()+" Message: "+playerMessageOriginal);
             }
         }
     }
